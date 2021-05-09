d02e99d2b12f3dbc7d35f8177970f526bfbb16ee
Kalpana <kodekittu@users.noreply.github.com>
2021-05-09 13:24:31 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/smart_traffic_management/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.smart_traffic_management
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/secrets.dart
@@ -0,0 +1,4 @@
+class Secrets {
+  // Add your Google Maps API Key here
+  static const API_KEY = 'AIzaSyCBggYmpyroj3QqY4Bpquu4drtjO4egrX8';
+}
